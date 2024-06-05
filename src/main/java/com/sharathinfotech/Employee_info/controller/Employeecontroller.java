03960ad5663cf94e4a62453913a556cbac6ca9d8
9182092380 <pothulasharath452@gmail.com>
2024-06-05 15:41:21 +0530

    first commit

---
+++ src/main/java/com/sharathinfotech/Employee_info/controller/Employeecontroller.java
@@ -0,0 +1,21 @@
+package com.sharathinfotech.Employee_info.controller;
+
+import com.sharathinfotech.Employee_info.Model.Employee;
+import com.sharathinfotech.Employee_info.service.Employeeservice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
+public class Employeecontroller {
+    @Autowired
+    private Employeeservice service;
+
+    @PostMapping("/addemp")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        return service.addEmployee(employee);
+    }
+}
